cf4e41ea36780f2a407fe6a04206ce977d3d28eb
mlabeg <100882596+mlabeg@users.noreply.github.com>
2023-05-12 16:30:00 +0200

    update wszystko bez gwiazdki, zostalo kilka todo

---
+++ 4_powtorka/Library/Library/Logic/Library.Domain/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain
+{
+    //public enum BookState :ushort
+    //{
+    //    //available =0,
+    //    //unavailable =1
+          
+    //}
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+        public string ISBN { get; set; }
+
+        public int ProductsAvailable { get; set; }
+        public decimal Price { get; set; }
+
+        public int State { get; set; }
+       // BookState State { get; set; } //0 - unavaliable, 1 - avaliable
+
+        public Book()
+        {
+            //State = BookState.avaliable;
+            State = 1;
+        }
+
+        public Book(string title, string author, int publicationYear, string isbn, int productsAvailable, decimal price)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            PublicationYear = publicationYear;
+            ProductsAvailable = productsAvailable;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title} Author: {Author} ProductsAvailable: {ProductsAvailable}";
+        }
+    }
+}
---
+++ 4_powtorka/Library/Library/Logic/Library.Persistence/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Library.Persistence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Library.Persistence")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("c222f882-e81d-422d-a739-6f60ed19510b")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
